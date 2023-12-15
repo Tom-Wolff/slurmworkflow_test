8e3ec8f5b38fcd4c6b0ebc3a114b84f47eaeaafb
Tom Wolff <tom.wolff@northwestern.edu>
2023-12-15 14:06:23 -0600

    Added in user specifications

--- 0-settings.R
+++ 0-settings.R
@@ -10,8 +10,20 @@ load_r_sh <- c(
 #   the partition you use
 #   the account to use (not on all system)
 hpc_config <- list(
-  "mail-user" = "<user>@<provider>",
-  "partition" = "epimodel",
-  # "account"   = "csde"
+  "mail-user" = "tom.wolff@northwestern.edu",
+  "partition" = "normal",
+  "account"   = "p32153",
   "mail-type" = "FAIL"
 )
+
+
+#SBATCH --account=p32153  ## YOUR ACCOUNT pXXXX or bXXXX
+#SBATCH --partition=normal  ### PARTITION (buyin, short, normal, etc)
+#SBATCH --nodes=1 ## how many computers do you need
+#SBATCH --ntasks-per-node=1 ## how many cpus or processors do you need on each computer
+#SBATCH --time=08:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
+#SBATCH --mem-per-cpu=1500M
+#SBATCH --job-name=va99 ## When you run squeue -u NETID this is how you can identify the job
+#SBATCH --output=va99_quest.out ## standard out and standard error goes to this file
+#SBATCH --mail-type=ALL ## you can receive e-mail alerts from SLURM when your job begins and when your job finishes (complet$
+#SBATCH --mail-user=srimer@northwestern.edu ## your email
